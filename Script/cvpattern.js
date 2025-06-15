135172424b85d08c4f6e52c5785dd1303ed2505c
Duy <tranquangduy15122006@gmail.com>
2025-06-15 23:39:36 +0700

    update: link

--- Script/cvpattern.js
+++ Script/cvpattern.js
@@ -1,10 +1,54 @@
-const { createElement } = require("react");
-
-function fetchCV() {
-    fetch()
-        .then(res => res.blob())
-        .then(blob => {
-            const url = URL.createObjectURL(blob);
-            const iframe = createElement('iframe');
-        });
+function getCVFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('cv');
 }
+
+function fetchCV(url_html, iframe_id) {
+    fetch(url_html)
+        .then(res => res.text())
+        .then(html  => {
+            const iframe = document.getElementById(iframe_id);
+            iframe.srcdoc = html;
+
+            iframe.onload = function() {
+                try {
+                    const doc = iframe.contentDocument || iframe.contentWindow.document;
+                    const style = doc.createElement('style');
+                        style.textContent = `
+                            html, body {
+                                margin: 0;
+                                padding: 0;
+                                width: 100%;
+                                height: 100%;
+                                box-sizing: border-box;
+                            }
+                        `;
+                    doc.head.appendChild(style);
+                    const link = doc.createElement('link');
+                    doc.head.appendChild(link);
+                } catch(err) {
+                    console.warn("Không inject được CSS vì iframe bị khác domain hoặc sandbox",err);
+                }
+            }
+        })
+        .catch(err => console.error(err));
+}
+
+window.onload = function() {
+    const cv = getCVFromURL();
+    if (cv === 'cv1') {
+        fetchCV('../Cvs/cv1.html', 'sample-cv');
+    }
+    if (cv === 'cv2') {
+        fetchCV('../Cvs/cv2.html', 'sample-cv');
+    }
+    if (cv === 'cv3') {
+        fetchCV('../Cvs/cv4.html', 'sample-cv');
+    }
+    if (cv === 'cv4') {
+        fetchCV('../Cvs/cv5.html', 'sample-cv');
+    }
+    if (cv === 'cv5') {
+        fetchCV('../Cvs/cv3.html', 'sample-cv');
+    }
+};
